0f71f9cf61f7ac7d147d1c690cf241a4231a5ca3
HyperSnacks <136746075+HyperSnacks@users.noreply.github.com>
2026-09-11 10:54:57 +0200

    idk what changed lol

--- apex_management_sys/obj/Debug/net10.0-windows/apex_management_sys.AssemblyInfo.cs
+++ apex_management_sys/obj/Debug/net10.0-windows/apex_management_sys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apex_management_sys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c55c707a5f2f62166a99517e2516f392b2f4e34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c499cf5f51913cff4e2c64e3b28e60f7a3a86b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("apex_management_sys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apex_management_sys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
