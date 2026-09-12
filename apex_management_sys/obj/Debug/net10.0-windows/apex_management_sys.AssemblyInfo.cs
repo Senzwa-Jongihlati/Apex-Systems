1c2ab59b53c0ddde80ce21e885c39827f8424d44
Lisa M <lisamdubo@gmail.com>
2026-09-12 15:36:28 +0200

    Ive added Form5 with no errors

--- apex_management_sys/obj/Debug/net10.0-windows/apex_management_sys.AssemblyInfo.cs
+++ apex_management_sys/obj/Debug/net10.0-windows/apex_management_sys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apex_management_sys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b79935369f4abc3a1936d28316abe55e4e8e7ce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55e68e6dfeaacd84c1884518e0b98283324b9dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("apex_management_sys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apex_management_sys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
