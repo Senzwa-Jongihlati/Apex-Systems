f9161c57a20681c341d58be1b41fb934d8141516
HyperSnacks <136746075+HyperSnacks@users.noreply.github.com>
2026-09-21 13:03:45 +0200

    login page changes

--- apex_management_sys/Login.Designer.cs
+++ apex_management_sys/Login.Designer.cs
@@ -29,171 +29,180 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            panel2 = new Panel();
             panel1 = new Panel();
-            linkLabel1 = new LinkLabel();
+            Password = new Panel();
+            txtPassword = new Krypton.Toolkit.KryptonTextBox();
             pictureBox2 = new PictureBox();
+            checkBox1 = new CheckBox();
+            Username = new Panel();
+            txtUsername = new Krypton.Toolkit.KryptonTextBox();
             pictureBox1 = new PictureBox();
             button1 = new Button();
-            txtPassword = new TextBox();
-            label2 = new Label();
-            label1 = new Label();
-            gtxtUsername = new TextBox();
-            panel2.SuspendLayout();
+            Background = new PictureBox();
             panel1.SuspendLayout();
+            Password.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            Username.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Background).BeginInit();
             SuspendLayout();
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(11, 61, 92);
-            panel2.BackgroundImage = Properties.Resources.Screenshot_2026_09_17_132434;
-            panel2.Controls.Add(panel1);
-            panel2.Dock = DockStyle.Fill;
-            panel2.Location = new Point(0, 0);
-            panel2.Margin = new Padding(2, 2, 2, 2);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(911, 419);
-            panel2.TabIndex = 1;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.FromArgb(11, 61, 92);
+            panel1.Anchor = AnchorStyles.None;
+            panel1.BackColor = Color.FromArgb(61, 137, 196);
             panel1.BackgroundImageLayout = ImageLayout.Stretch;
-            panel1.BorderStyle = BorderStyle.Fixed3D;
-            panel1.Controls.Add(linkLabel1);
-            panel1.Controls.Add(pictureBox2);
-            panel1.Controls.Add(pictureBox1);
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(Password);
+            panel1.Controls.Add(Username);
             panel1.Controls.Add(button1);
-            panel1.Controls.Add(txtPassword);
-            panel1.Controls.Add(label2);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(gtxtUsername);
-            panel1.Location = new Point(0, 2);
-            panel1.Margin = new Padding(2, 2, 2, 2);
+            panel1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel1.ForeColor = Color.Black;
+            panel1.Location = new Point(200, 261);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(213, 419);
+            panel1.Size = new Size(470, 448);
             panel1.TabIndex = 0;
             // 
-            // linkLabel1
+            // Password
             // 
-            linkLabel1.AutoSize = true;
-            linkLabel1.LinkColor = Color.White;
-            linkLabel1.Location = new Point(22, 211);
-            linkLabel1.Margin = new Padding(2, 0, 2, 0);
-            linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(89, 15);
-            linkLabel1.TabIndex = 9;
-            linkLabel1.TabStop = true;
-            linkLabel1.Text = "Show Password";
-            linkLabel1.UseMnemonic = false;
+            Password.Controls.Add(txtPassword);
+            Password.Controls.Add(pictureBox2);
+            Password.Controls.Add(checkBox1);
+            Password.Location = new Point(45, 253);
+            Password.Name = "Password";
+            Password.Size = new Size(376, 54);
+            Password.TabIndex = 13;
+            // 
+            // txtPassword
+            // 
+            txtPassword.CueHint.CueHintText = "Enter Password";
+            txtPassword.Dock = DockStyle.Fill;
+            txtPassword.Location = new Point(23, 0);
+            txtPassword.MaxLength = 250;
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '●';
+            txtPassword.Size = new Size(353, 27);
+            txtPassword.TabIndex = 15;
+            txtPassword.UseSystemPasswordChar = true;
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(2, 185);
-            pictureBox2.Margin = new Padding(2, 2, 2, 2);
+            pictureBox2.Dock = DockStyle.Left;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Margin = new Padding(2);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(19, 17);
+            pictureBox2.Size = new Size(23, 30);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 8;
             pictureBox2.TabStop = false;
             // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Dock = DockStyle.Bottom;
+            checkBox1.Location = new Point(0, 30);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(376, 24);
+            checkBox1.TabIndex = 14;
+            checkBox1.Text = "Show Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Username
+            // 
+            Username.Controls.Add(txtUsername);
+            Username.Controls.Add(pictureBox1);
+            Username.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Username.Location = new Point(45, 148);
+            Username.Name = "Username";
+            Username.Size = new Size(376, 30);
+            Username.TabIndex = 10;
+            // 
+            // txtUsername
+            // 
+            txtUsername.CueHint.CueHintText = "Enter Username";
+            txtUsername.Dock = DockStyle.Fill;
+            txtUsername.Location = new Point(22, 0);
+            txtUsername.MaxLength = 250;
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(354, 27);
+            txtUsername.TabIndex = 14;
+            // 
             // pictureBox1
             // 
+            pictureBox1.Dock = DockStyle.Left;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(2, 58);
-            pictureBox1.Margin = new Padding(2, 2, 2, 2);
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(19, 17);
+            pictureBox1.Size = new Size(22, 30);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 7;
             pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            button1.BackColor = Color.FromArgb(11, 61, 92);
+            button1.BackColor = Color.FromArgb(230, 235, 240);
             button1.FlatStyle = FlatStyle.Popup;
             button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(22, 317);
-            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(45, 350);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(78, 20);
+            button1.Size = new Size(144, 48);
             button1.TabIndex = 4;
             button1.Text = "Login";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click_1;
             // 
-            // txtPassword
+            // Background
             // 
-            txtPassword.Location = new Point(22, 183);
-            txtPassword.Margin = new Padding(2, 2, 2, 2);
-            txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(106, 23);
-            txtPassword.TabIndex = 3;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(25, 145);
-            label2.Margin = new Padding(2, 0, 2, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(59, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Password";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(22, 32);
-            label1.Margin = new Padding(2, 0, 2, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(64, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Username";
-            // 
-            // gtxtUsername
-            // 
-            gtxtUsername.Location = new Point(22, 58);
-            gtxtUsername.Margin = new Padding(2, 2, 2, 2);
-            gtxtUsername.Name = "gtxtUsername";
-            gtxtUsername.Size = new Size(106, 23);
-            gtxtUsername.TabIndex = 0;
+            Background.Dock = DockStyle.Fill;
+            Background.Image = Properties.Resources.Screenshot_2026_09_17_132434;
+            Background.Location = new Point(0, 0);
+            Background.Name = "Background";
+            Background.Size = new Size(1291, 799);
+            Background.SizeMode = PictureBoxSizeMode.StretchImage;
+            Background.TabIndex = 1;
+            Background.TabStop = false;
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(911, 419);
-            Controls.Add(panel2);
+            ClientSize = new Size(1291, 799);
+            Controls.Add(panel1);
+            Controls.Add(Background);
+            ForeColor = SystemColors.ControlText;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             WindowState = FormWindowState.Maximized;
-            panel2.ResumeLayout(false);
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            Password.ResumeLayout(false);
+            Password.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            Username.ResumeLayout(false);
+            Username.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Background).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private Panel panel2;
         private Panel panel1;
         private PictureBox pictureBox2;
         private PictureBox pictureBox1;
         private Button button1;
-        private TextBox txtPassword;
-        private Label label2;
+        private PictureBox Background;
+        private Panel Username;
         private Label label1;
-        private TextBox gtxtUsername;
-        private LinkLabel linkLabel1;
+        private CheckBox checkBox1;
+        private Panel Password;
+        private Krypton.Toolkit.KryptonTextBox txtPassword;
+        private Krypton.Toolkit.KryptonTextBox txtUsername;
     }
 }
